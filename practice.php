84fe3c0cbd5b67a73ad67acc9751d3fd0e6040e5
Andre Mander <andre.mander@gmail.com>
2017-10-25 21:23:41 -0400

    added practice of object variable and arrays

--- practice.php
+++ practice.php
@@ -1,28 +1,27 @@
 <?php
-$title = 'Recipe Box';
-$recipe = 'Rice';
-$ingredients = 'Ingredients';
-$ingredient1 = '2 cups rice';
-$ingredient2 = '1 cup water';
-$instructions = '(1) Wash the rice.<br>
-(2) Combine ingredients in a pot. Then cook on stove for 30 minutes.';
+$object = array(
+	'title' => 'Recipe Box',
+	'recipe' => 'Rice',
+	'ingredients' => '2 cups rice and<br>1 cup water',
+	'instructions' => '(1) Wash the rice.<br>
+(2) Combine ingredients in a pot. Then cook on stove for 30 minutes.',
+);
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title><?php echo $title;?></title>
+	<title><?php echo $object ['title'];?> </title>
 </head>
 <body>
-	<h1><?php echo $recipe; ?></h1>
+
+	<h1><?php echo $object ['recipe']; ?> </h1>
 	<hr>
-	<h2><?php echo $ingredients;?></h2>
-	<ul>
-		<li><?php echo $ingredient1; ?></li>
-		<li><?php echo $ingredient2; ?></li>
-	</ul>
+	<h2>Ingredients</h2>
+	<p><?php echo $object ['ingredients'];?> </p>
 	<hr>
 	<h2>Instructions</h2>
-	<p><?php echo $instructions; ?></p>
+	<p><?php echo $object ['instructions']; ?> </p>
 	
 
 </body>
